147d0b43612377ab49ac35a1b0830e32e4f247d2
C Bares <colinedwinbares@gmail.com>
2021-12-16 13:17:52 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,7 +97,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -194,16 +194,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function(event) {
-    
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
-    
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
-    
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -242,14 +244,14 @@ $('#trash').droppable({
   accept: '.card .list-group-item',
   tolerance: 'touch',
   drop: function(event, ui) {
-    console.log('drop');
+    $('.bottom-trash').removeClass('bottom-trash-active');    
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log('over');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log('out');
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 
@@ -278,4 +280,8 @@ var auditTask = function(taskEl) {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000)
